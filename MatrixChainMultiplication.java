2e82008723c228fc8d193fb86c527bdf3ed5c478
prithindevotta <59787151+prithindevotta@users.noreply.github.com>
2021-10-05 13:09:04 +0530

    7th commit

--- MatrixChainMultiplication.java
+++ MatrixChainMultiplication.java
@@ -1,4 +1,16 @@
 package cp;
 
 public class MatrixChainMultiplication {
+    public static class Pairs{
+        int first;
+        int last;
+
+        public Pairs(int first, int last){
+            this.first = first;
+            this.last = last;
+        }
+    }
+    public static void main(String[] args){
+
+    }
 }
